6076ba1e826003cf1405acd6250aa09c5fb387b7
dlaghxkr-oss <dlaghxkr@gmail.com>
2026-08-25 15:12:57 +0900

    260825 property v2 tak

---
+++ src/main/java/com/zipdaprojecttak/ZIpDaProjectTakApplication.java
@@ -0,0 +1,13 @@
+package com.zipdaprojecttak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZIpDaProjectTakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZIpDaProjectTakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zipdaprojecttak/ZIpDaProjectTakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipdaprojecttak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZIpDaProjectTakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
