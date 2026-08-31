745e35d60af03276c439e4c802dd0a95f7a32107
수린 <soo970220@gmail.com>
2026-08-31 12:34:24 +0900

    20260828 | feat: 찜 생성 로직 및 집계 조회 추가

---
+++ src/main/java/com/zipdaproperty/domain/property/Service/PropertyFavoriteService.java
@@ -0,0 +1,9 @@
+package com.zipdaproperty.domain.property.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PropertyFavoriteService {
+}
